0084e9fb7e53a435e9e2f2a9fde0702698794af0
Sarthak Girdhar <sg.sarthakgirdhar@gmail.com>
2015-04-30 17:38:17 +0530

    Create whats-your-name.py

---
+++ hacherrank/whats-your-name/whats-your-name.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+firstname = raw_input()
+lastname = raw_input()
+
+print "Hello " + str (firstname) + " " + str (lastname) + "! You just delved into python."
