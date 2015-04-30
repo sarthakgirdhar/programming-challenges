d1752e8982fed20cb11ff791e6a793c7ac94170e
Sarthak Girdhar <sg.sarthakgirdhar@gmail.com>
2015-04-30 19:49:09 +0530

    Create basic calculator.py

---
+++ hacherrank/basic-calculator/basic calculator.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = float(raw_input())
+b = float(raw_input())
+
+print "%.2f" %(a + b)
+print "%.2f" %(a - b)
+print "%.2f" %(a * b)
+print "%.2f" %(a / b)
+print "%.2f" %(a // b)
